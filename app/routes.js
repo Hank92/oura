3147060539d3278ce62ec2834f7c072989205d00
OWNER1 <hongjik92@daum.net>
2016-05-05 18:30:13 +0900

    add statcounter and deleted unused page

--- app/routes.js
+++ app/routes.js
@@ -44,24 +44,6 @@ app.get('/', function (req, res){
 	
 });
 
-
-app.get('/issuetodaydelete', function (req, res){
-	issuetodayModel.find({}, function(req, docs){
-		res.render('issuetodaydelete.ejs', {postModels: docs})	
-	})
-	
-})
-
-
-app.get('/issuetodaydelete/:id/delete', function(req, res){
-	issuetodayModel.remove({_id: req.params.id}, 
-	   function(err){
-		if(err) res.json(err);
-		else    res.redirect('/issuetodaydelete');
-	});
-});
-
-
 app.get('/issuein', function (req, res){
 
 	var currentPage = 1;
@@ -90,7 +72,6 @@ app.get('/issuein', function (req, res){
      });//paginate
 	
 });
-
 
 
 app.param('id', function(req, res, next, id){
@@ -123,6 +104,7 @@ app.param('id', function(req, res, next, id){
 
 app.get('/:id', function(req, res){
 	   res.render('individualHazzulMain.ejs', {postModel: req.mainpostId});
+	   console.log(req.mainpostId)
 	})
 	
 	//finds the matching object
@@ -154,9 +136,6 @@ app.post('/:id/post', function (req, res){
 	})
 
 }) //app.post  
-
-
-
 };
 
 request('http://bhu.co.kr/bbs/board.php?bo_table=best&page=1', function(err, res, body){
@@ -227,200 +206,6 @@ request('http://bhu.co.kr/bbs/board.php?bo_table=best&page=1', function(err, res
 
 });
 
-request('http://issuetodays.me/bbs/board.php?bo_table=humor', function(err, res, body){
-	
-	if(!err && res.statusCode == 200) {
-		
-		var $ = cheerio.load(body);
-		$('li.gall_li').each(function(){
-		var issuetodayTitle = $(this).find('a').text();
-		var issuetodayUrl = $(this).find('a').attr('href');
-		
-	 	
-			request(issuetodayUrl, function(err, res, body){
-				if(!err && res.statusCode == 200) {
-				var $ = cheerio.load(body);
-		
-				var image_url = [];
-
-					$("#bo_v_atc a img").each(function(){
-						var img_url = $(this).attr('src');
-							image_url.push(img_url);	
-					})// scrape all the images for the post
-
-
-					$("#bo_v_atc [style *= 'text-align:center;']").each(function(){
-						var img_url = $(this).find('img').attr('src');
-							image_url.push(img_url);	
-					})// scrape all the images for the post
-					
-
-			issuetodayModel.find({title: issuetodayTitle}, function(err, newPosts){
-				
-				if (!newPosts.length){
-					//save data in Mongodb
-
-					var Post = new issuetodayModel({
-						title: issuetodayTitle,
-						url: issuetodayUrl,
-						image_url: image_url
-					})
-			Post.save(function(error){
-					if(error){
-						console.log(error);
-					}
-					else 
-						console.log(Post);
-				})
-
-			//post.save
-				}//if bhuTitle안에 있는 {}
-
-			})//postModel.find
-			
-
-			}//if문
-
-			})//request
-
-			
-		});
-		
-	}//첫 if구문
-
-});
-/*
-request('http://bbs2.ruliweb.daum.net/gaia/do/ruliweb/default/etc/2078/list?bbsId=G005&pageIndex=1&itemId=143&objCate1=497', function(err, res, body){
-	
-	if(!err && res.statusCode == 200) {
-		
-		var $ = cheerio.load(body);
-		$('td.subject').each(function(){
-		var ruliTitle = $(this).find('a').text();
-		var newHref = $(this).find('a').attr('href');
-		newHref = newHref.replace("/list?bbsId=G005&pageIndex=1&itemId=143&objCate1=497",newHref);
-		var ruliUrl = "http://bbs2.ruliweb.daum.net/gaia/do/ruliweb/default/etc/2078/"+ newHref;
-	 	
-			request(ruliUrl, function(err, res, body){
-				if(!err && res.statusCode == 200) {
-				var $ = cheerio.load(body);
-				var comments = [];
-				var image_url = [];
-
-				$('.read_cont_table p').each(function(){
-					var img_url = $(this).find('img').attr('src');
-					image_url.push(img_url);	
-				})
-				// scrape all the images for the post
-				
-					$("table td.cont").each(function(){
-						var content =  $(this).text();
-							comments.push({content: content}); 	
-					})//scrape all the comments for the post
-
-
-			postModel.find({title: ruliTitle}, function(err, newPosts){
-				
-				if (!newPosts.length){
-					//save data in Mongodb
-
-					var Post = new ruliModel({
-						title: ruliTitle,
-						url: ruliUrl,
-						image_url: image_url,
-						comments: comments
-					})
-			Post.save(function(error){
-					if(error){
-						console.log(error);
-					}
-					else 
-						console.log(Post);
-				})
-
-			//post.save
-				}//if bhuTitle안에 있는 {}
-
-			})//postModel.find
-			
-
-			}//if문
-
-			})//request
-
-			
-		});
-		
-	}//첫 if구문
-
-});
-/*
-request('http://ruliweb.daum.net/gallery/hit/article.daum', function(err, res, body){
-	
-	if(!err && res.statusCode == 200) {
-		
-		var $ = cheerio.load(body);
-		$('td.subject').each(function(){
-		var ruliTitle = $(this).find('a').text();
-		var ruliUrl= $(this).find('a').attr('href');
-	 	
-			request(ruliUrl, function(err, res, body){
-				if(!err && res.statusCode == 200) {
-				var $ = cheerio.load(body);
-				var comments = [];
-				var image_url = [];
-
-				$('.read_cont_table p').each(function(){
-					var img_url = $(this).find('img').attr('src');
-					image_url.push(img_url);	
-				})
-				// scrape all the images for the post
-				
-					$("#commentTable").each(function(){
-						var content =  $(this).find('td.cont').text();
-							comments.push({content: content}); 	
-					})//scrape all the comments for the post
-
-
-			postModel.find({title: ruliTitle}, function(err, newPosts){
-				
-				if (!newPosts.length){
-					//save data in Mongodb
-
-					var Post = new ruliModel({
-						title: ruliTitle,
-						url: ruliUrl,
-						image_url: image_url,
-						comments: comments
-					})
-			Post.save(function(error){
-					if(error){
-						console.log(error);
-					}
-					else 
-						console.log(Post);
-				})
-
-			//post.save
-				}//if bhuTitle안에 있는 {}
-
-			})//postModel.find
-			
-
-			}//if문
-
-			})//request
-
-			
-		});
-		
-	}//첫 if구문
-
-});
-*/
-
-
-
 request('http://issuein.com/', function(err, res, body){
 	
 	if(!err && res.statusCode == 200) {
@@ -479,71 +264,3 @@ request('http://issuein.com/', function(err, res, body){
 
 });
 
-
-request('http://bhu.co.kr/bbs/board.php?bo_table=free', function(err, res, body){
-	
-	if(!err && res.statusCode == 200) {
-		
-		var $ = cheerio.load(body);
-		$('td.subject').each(function(){
-		var bhuTitle = $(this).find('a font').text();
-		var newHref = $(this).find('a').attr('href');
-		newHref = newHref.replace("≀","&");
-		newHref = newHref.replace("id","wr_id");
-		newHref = newHref.replace("..",".");
-		var bhuUrl = "http://www.bhu.co.kr"+ newHref;
-	 	
-			request(bhuUrl, function(err, res, body){
-				if(!err && res.statusCode == 200) {
-				var $ = cheerio.load(body);
-				var comments = [];
-				var image_url = [];
-
-				$('span div img').each(function(){
-					var img_url = $(this).attr('src');
-					image_url.push(img_url);	
-				})
-				// scrape all the images for the post
-				
-					$("[style *= 'line-height: 180%']").each(function(){
-						var content =  $(this).text();
-							comments.push({content: content}); 	
-					})//scrape all the comments for the post
-
-					comments.splice(0,1)
-
-			postModel.find({title: bhuTitle}, function(err, newPosts){
-				
-				if (!newPosts.length){
-					//save data in Mongodb
-
-					var Post = new postModel({
-						title: bhuTitle,
-						url: bhuUrl,
-						image_url: image_url,
-						comments: comments
-					})
-			Post.save(function(error){
-					if(error){
-						console.log(error);
-					}
-					else 
-						console.log(Post);
-				})
-
-			//post.save
-				}//if bhuTitle안에 있는 {}
-
-			})//postModel.find
-			
-
-			}//if문
-
-			})//request
-
-			
-		});
-		
-	}//첫 if구문
-
-});
